871a9f5531e6648c11c5c1944bb8932eb8bb07f9
Serpent1776 <j.d.hemling@email.msmary.edu>
2025-03-28 15:50:12 -0400

    weather bugs fixed

--- Py/HW10/weatherxml.py
+++ Py/HW10/weatherxml.py
@@ -2,6 +2,12 @@
 import requests
 import xml.etree.ElementTree as ET
 
-text = requests.get("https://forecast.weather.gov/MapClick.php?lat=39.7027&lon=-77.327&unit=0&lg=english&FcstType=dwml%27).text").text
-#v = ET.fromstring(text) #this causes an error. How am I supposed to do this? This is not explained in class.
-#print(text)
+text = requests.get("https://forecast.weather.gov/MapClick.php?lat=39.7027&lon=-77.327&unit=0&lg=english&FcstType=dwml").text
+#print(text) debug
+v = ET.fromstring(text) 
+#print(v) debug
+params = v.find("data").find("parameters").find("wordedForecast")
+#print(params) debug
+
+for f in params.findall("text"):
+    print(f.text)
